9498b5d561f337ab7fc25706f88a7c0eeec6665e
NjordJ <nj0rd3841@gmail.com>
2023-10-20 12:32:41 +0500

    Screens for deposit

--- app/src/main/java/com/irudaru/depcalculator/MainActivity.kt
+++ app/src/main/java/com/irudaru/depcalculator/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.irudaru.depcalculator.ui.home.screens.HomeScreen
 import com.irudaru.depcalculator.ui.theme.DepCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,25 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "DepCalculator",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DepCalculatorTheme {
-        Greeting()
     }
 }
---
+++ app/src/main/java/com/irudaru/depcalculator/ui/depositlist/screens/DepositListScreen.kt
@@ -0,0 +1,74 @@
+package com.irudaru.depcalculator.ui.depositlist.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.irudaru.depcalculator.ui.theme.DepCalculatorTheme
+
+@Composable
+fun DepositListScreen(modifier: Modifier = Modifier){
+    LazyColumn(
+        modifier = Modifier
+            .padding(4.dp)
+    ) {
+        items(5) { index ->
+            DepositCard(
+                index = index,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun DepositCard(index: Int, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+    ) {
+        DepositCardContent(
+            index = index,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+private fun DepositCardContent(index: Int, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(modifier = Modifier.padding(vertical = 4.dp)) {
+            Text(
+                text = "Deposit $index"
+            )
+            Text(
+                text = "5%",
+                modifier = Modifier.wrapContentWidth(Alignment.End)
+            )
+        }
+        Row(modifier = Modifier.padding(vertical = 4.dp)) {
+            Text(text = "10000$")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+private fun DepositListScreenPreview() {
+    DepCalculatorTheme {
+        DepositListScreen()
+    }
+}
